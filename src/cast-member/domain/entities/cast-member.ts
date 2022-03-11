c7af7e9aa62dc2d4b9472c2513eaf94275a935f9
Danny França <danny_vcf@hotmail.com>
2022-03-11 17:44:10 -0300

    feat: add cast member domain

---
+++ src/cast-member/domain/entities/cast-member.ts
@@ -0,0 +1,52 @@
+import UniqueEntityId from '@/@seedwork/domain/unique-entity-id';
+import { SetOptional } from 'type-fest';
+
+export enum CastMemberType {
+  DIRECTOR = 'DIRECTOR',
+  ACTOR = 'ACTOR',
+}
+
+export type CastMemberProperties = {
+  name: string;
+  type: CastMemberType;
+  created_at: Date;
+};
+
+export type CastMemberPropertiesInput = SetOptional<
+  CastMemberProperties,
+  'type' | 'created_at'
+>;
+
+export class CastMember {
+  public readonly id: UniqueEntityId;
+  private _name: string;
+  private _type: CastMemberType;
+  private _created_at: Date;
+
+  constructor(props: CastMemberPropertiesInput, id?: UniqueEntityId) {
+    this.id = id ?? new UniqueEntityId();
+    this._name = props.name;
+    this._type = props.type ?? CastMemberType.ACTOR;
+    this._created_at = props.created_at ?? new Date();
+  }
+
+  get props(): CastMemberProperties {
+    return {
+      name: this.name,
+      type: this.type,
+      created_at: this.created_at,
+    };
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get type() {
+    return this._type;
+  }
+
+  get created_at() {
+    return this._created_at;
+  }
+}
