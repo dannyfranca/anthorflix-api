b19caca4732e0de006c1a5d8030aa746785d0830
Danny França <danny_vcf@hotmail.com>
2022-03-11 21:17:07 -0300

    feat: aggregate genre and cast memebrs to movie entity

--- src/domains/movie/entities/movie.ts
+++ src/domains/movie/entities/movie.ts
@@ -1,12 +1,16 @@
 import { SetOptional } from 'type-fest';
 
 import UniqueEntityId from '@/@seedwork/domain/unique-entity-id';
+import { CastMember } from '@/domains/cast-members/entities/cast-member';
+import { Genre } from '@/domains/genre/entities/genre';
 
 export type MovieProperties = {
   title: string;
   description: string;
   year_launched: number;
   created_at: Date;
+  cast_members: CastMember[];
+  genres: Genre[];
 };
 
 export type MoviePropertiesInput = SetOptional<MovieProperties, 'created_at'>;
@@ -17,12 +21,16 @@ export class Movie {
   private _description: string;
   private _year_launched: number;
   private _created_at: Date;
+  private _cast_members: CastMember[];
+  private _genres: Genre[];
 
   constructor(props: MoviePropertiesInput, id?: UniqueEntityId) {
     this.id = id ?? new UniqueEntityId();
     this._title = props.title;
     this._description = props.description;
     this._year_launched = props.year_launched;
+    this._cast_members = props.cast_members;
+    this._genres = props.genres;
     this._created_at = props.created_at ?? new Date();
   }
 
@@ -31,6 +39,8 @@ export class Movie {
       title: this.title,
       description: this.description,
       year_launched: this.year_launched,
+      cast_members: this.cast_members,
+      genres: this.genres,
       created_at: this.created_at,
     };
   }
@@ -45,6 +55,14 @@ export class Movie {
 
   get year_launched() {
     return this._year_launched;
+  }
+
+  get cast_members() {
+    return this._cast_members;
+  }
+
+  get genres() {
+    return this._genres;
   }
 
   get created_at() {
