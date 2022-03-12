3cf62a18d3904cae4bacff5a6ef45f20398ca125
Danny França <danny_vcf@hotmail.com>
2022-03-12 14:16:33 -0300

    feat: add prisma test controller

---
+++ src/@seedwork/infra/prisma-test-controller.spec.ts
@@ -0,0 +1,17 @@
+import { PrismaTestController } from './prisma-test-controller';
+import { PrismaService } from './prisma.service';
+
+describe('PrismaTestController', () => {
+  test('should not trigger Error', async () => {
+    const prismaTestController = new PrismaTestController();
+    const url = prismaTestController.url;
+
+    expect(prismaTestController.prisma).toBeInstanceOf(PrismaService);
+    expect(prismaTestController.url).toBe(url);
+
+    await prismaTestController.init();
+    expect(prismaTestController.url).not.toBe(url);
+
+    await prismaTestController.destroy();
+  });
+});
