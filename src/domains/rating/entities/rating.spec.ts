955f67ab27105becb66cc5e0a0f10252e1a9706b
Danny França <danny_vcf@hotmail.com>
2022-03-11 21:00:04 -0300

    feat: aggregate comment to rating domain

--- src/domains/rating/entities/rating.spec.ts
+++ src/domains/rating/entities/rating.spec.ts
@@ -3,9 +3,20 @@ import { omit } from 'lodash';
 import UniqueEntityId from '@/@seedwork/domain/unique-entity-id';
 import { Rating, RatingProperties } from './rating';
 import { User } from '@/domains/user/entities/user';
+import { Comment } from '@/domains/comment/entities/comment';
 
 const content = 'Some rating content';
-const generateUser = () => new User({ username: 'some_user' });
+const generateUser = () => new User({ username: 'rating_user' });
+const generateComments = () => [
+  new Comment({
+    content: 'Comment form user 1',
+    user: new User({ username: 'comment_user1' }),
+  }),
+  new Comment({
+    content: 'Comment form user 2',
+    user: new User({ username: 'comment_user2' }),
+  }),
+];
 
 describe('Rating Tests', () => {
   const validateSpy = jest.spyOn(Rating.prototype as any, 'validate');
@@ -13,7 +24,8 @@ describe('Rating Tests', () => {
   beforeEach(() => validateSpy.mockClear());
   test('rating constructor', () => {
     const user = generateUser();
-    let rating = new Rating({ value: 5, user });
+    const comments = generateComments();
+    let rating = new Rating({ value: 5, user, comments });
     let created_at: Date;
 
     const props = omit(rating.props, 'created_at');
@@ -21,6 +33,7 @@ describe('Rating Tests', () => {
       value: 5,
       content: null,
       user,
+      comments,
     } as RatingProperties);
     expect(rating.created_at).toBeInstanceOf(Date);
 
@@ -30,12 +43,14 @@ describe('Rating Tests', () => {
       value: 4,
       created_at,
       user,
+      comments,
     });
     expect(rating.props).toStrictEqual({
       content,
       value: 4,
       created_at,
       user,
+      comments,
     } as RatingProperties);
 
     created_at = new Date();
@@ -44,6 +59,7 @@ describe('Rating Tests', () => {
       value: 5,
       created_at,
       user,
+      comments,
     });
     expect(rating.props).toMatchObject({
       content: 'Another content',
@@ -53,25 +69,19 @@ describe('Rating Tests', () => {
     expect(validateSpy).toHaveBeenCalled();
   });
 
-  it('should throw error on contructor', () => {
-    const user = generateUser();
-    expect(() => new Rating({ value: 11, user }));
-    expect(() => new Rating({ value: 3.5, user }));
-    expect(() => new Rating({ value: -1, user }));
-  });
-
   test('rating getters', () => {
     const user = generateUser();
+    const comments = generateComments();
     let rating: Rating;
     const created_at = new Date();
 
-    rating = new Rating({ content, value: 6, user });
+    rating = new Rating({ content, value: 6, user, comments });
     expect(rating.content).toBe(content);
     expect(rating.value).toBe(6);
     expect(rating.user.props).toStrictEqual(user.props);
     expect(rating.created_at).toBeInstanceOf(Date);
 
-    rating = new Rating({ value: 4, created_at, user });
+    rating = new Rating({ value: 4, created_at, user, comments });
     expect(rating.content).toBeNull();
     expect(rating.value).toBe(4);
     expect(rating.user.props).toStrictEqual(user.props);
@@ -81,15 +91,31 @@ describe('Rating Tests', () => {
 
   test('id field', () => {
     const user = generateUser();
+    const comments = generateComments();
     let rating: Rating;
     const uniqueId = new UniqueEntityId();
 
-    rating = new Rating({ value: 4, user });
+    rating = new Rating({ value: 4, user, comments });
     expect(rating.id).toBeInstanceOf(UniqueEntityId);
 
-    rating = new Rating({ value: 4, user }, uniqueId);
+    rating = new Rating({ value: 4, user, comments }, uniqueId);
     expect(rating.id).toBeInstanceOf(UniqueEntityId);
     expect(rating.id).toBe(uniqueId);
     expect(rating.id.id).toBe(uniqueId.id);
+  });
+
+  it('should throw error on contructor', () => {
+    const user = generateUser();
+    const comments = generateComments();
+    expect(() => new Rating({ value: 11, user, comments }));
+    expect(() => new Rating({ value: 3.5, user, comments }));
+    expect(() => new Rating({ value: -1, user, comments }));
+  });
+
+  it('should count comments', () => {
+    const user = generateUser();
+    const comments = generateComments();
+    const rating = new Rating({ value: 9, user, comments });
+    expect(rating.commentCount()).toBe(2);
   });
 });
--- src/domains/rating/entities/rating.ts
+++ src/domains/rating/entities/rating.ts
@@ -4,12 +4,14 @@ import { isInteger } from 'lodash';
 import UniqueEntityId from '@/@seedwork/domain/unique-entity-id';
 import InvalidRatingError from '@/domains/rating/errors/invalid-rating.error';
 import { User } from '@/domains/user/entities/user';
+import { Comment } from '@/domains/comment/entities/comment';
 
 export type RatingProperties = {
   value: number;
   content: string | null;
   created_at: Date;
   user: User;
+  comments: Comment[];
 };
 
 export type RatingPropertiesInput = SetOptional<
@@ -23,11 +25,13 @@ export class Rating {
   private _content: string | null;
   private _created_at: Date;
   private _user: User;
+  private _comments: Comment[];
 
   constructor(props: RatingPropertiesInput, id?: UniqueEntityId) {
     this.id = id ?? new UniqueEntityId();
     this._value = props.value;
     this._user = props.user;
+    this._comments = props.comments;
     this._content = props.content ?? null;
     this._created_at = props.created_at ?? new Date();
     this.validate();
@@ -39,6 +43,7 @@ export class Rating {
       content: this.content,
       user: this.user,
       created_at: this.created_at,
+      comments: this.comments,
     };
   }
 
@@ -54,8 +59,16 @@ export class Rating {
     return this._user;
   }
 
+  get comments() {
+    return this._comments;
+  }
+
   get created_at() {
     return this._created_at;
+  }
+
+  commentCount() {
+    return this.comments.length;
   }
 
   validate() {
