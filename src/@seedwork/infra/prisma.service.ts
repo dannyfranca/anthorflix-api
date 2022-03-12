d527ca3443c24eaa33ded82940f509c20333550f
Danny França <danny_vcf@hotmail.com>
2022-03-12 00:31:54 -0300

    fix: remove unecessary prisma hook onModuleDestroy

--- src/@seedwork/infra/prisma.service.ts
+++ src/@seedwork/infra/prisma.service.ts
@@ -7,16 +7,9 @@ import {
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
-  }
-
-  async onModuleDestroy() {
-    await this.$disconnect();
   }
 
   async enableShutdownHooks(app: INestApplication) {
