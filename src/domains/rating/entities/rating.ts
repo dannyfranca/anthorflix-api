4f6d8acccd3e8dd7dce2076973c75af01f603cdd
Danny França <danny_vcf@hotmail.com>
2022-03-14 16:43:09 -0300

    feat: rating must extend base entity

--- src/domains/rating/entities/rating.ts
+++ src/domains/rating/entities/rating.ts
@@ -1,53 +1,61 @@
-import { SetOptional } from 'type-fest';
 import { isInteger } from 'lodash';
 
 import UniqueEntityId from '@/@seedwork/entities/unique-entity-id';
 import InvalidRatingError from '@/domains/rating/errors/invalid-rating.error';
 import { User } from '@/domains/user/entities/user';
-import { Comment } from '@/domains/comment/entities/comment';
+import {
+  Entity,
+  EntityProperties,
+  EntityPropertiesInput,
+  PlainEntity,
+} from '@/@seedwork/entities/entity';
+import Description from '@/@seedwork/entities/description';
+import { SetOptional } from 'type-fest';
 
-export type RatingProperties = {
+interface RatingOnlyProperties {
   value: number;
-  content: string | null;
-  created_at: Date;
+  content: Description | null;
   user: User;
   movie_id: UniqueEntityId;
-  comments: Comment[];
-};
+}
 
-export type RatingPropertiesInput = SetOptional<
-  RatingProperties,
-  'content' | 'created_at'
->;
+export interface RatingProperties
+  extends EntityProperties,
+    RatingOnlyProperties {}
 
-export class Rating {
-  public readonly id: UniqueEntityId;
+export interface RatingPropertiesInput
+  extends EntityPropertiesInput,
+    SetOptional<RatingOnlyProperties, 'content'> {}
+
+export interface PlainRating extends PlainEntity {
+  value: number;
+  content: string | null;
+  movie_id: string;
+  user_id: string;
+}
+
+export class Rating extends Entity {
   private _value: number;
-  private _content: string | null;
-  private _created_at: Date;
+  private _content: Description | null;
   private _user: User;
   private _movie_id: UniqueEntityId;
-  private _comments: Comment[];
 
-  constructor(props: RatingPropertiesInput, id?: UniqueEntityId) {
-    this.id = id ?? new UniqueEntityId();
+  constructor(props: RatingPropertiesInput) {
+    super(props);
     this._value = props.value;
     this._user = props.user;
     this._movie_id = props.movie_id;
-    this._comments = props.comments;
     this._content = props.content ?? null;
-    this._created_at = props.created_at ?? new Date();
     this.validate();
   }
 
-  get props(): RatingProperties {
+  get plain(): PlainRating {
     return {
+      ...super.plain,
       value: this.value,
-      content: this.content,
-      user: this.user,
-      movie_id: this.movie_id,
-      created_at: this.created_at,
-      comments: this.comments,
+      content: this.content?.value ?? null,
+      user_id: this.user.id.value,
+      movie_id: this.movie_id.value,
     };
   }
 
@@ -65,18 +73,6 @@ export class Rating {
 
   get movie_id() {
     return this._movie_id;
-  }
-
-  get comments() {
-    return this._comments;
-  }
-
-  get created_at() {
-    return this._created_at;
-  }
-
-  commentCount() {
-    return this.comments.length;
   }
 
   validate() {
