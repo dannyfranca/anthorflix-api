d963b0403dccf656610b15f9da1c982063fe7a3d
Danny França <danny_vcf@hotmail.com>
2022-03-16 21:55:44 -0300

    fix: wrong build config

--- docker-build.sh
+++ docker-build.sh
@@ -2,7 +2,7 @@
 
 SHA=$(git rev-parse HEAD)
 
-docker build . -f Containerfile -t dannyfranca/anthorflix-api:latest -t dannyfranca/anthorflix-api:$SHA --target runtime
+docker build . -t dannyfranca/anthorflix-api:latest -t dannyfranca/anthorflix-api:$SHA --target runtime
 
 docker push dannyfranca/anthorflix-api:latest
 docker push dannyfranca/anthorflix-api:$SHA
